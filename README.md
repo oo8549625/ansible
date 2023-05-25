ab6150588081c886bb78ecb2d1c40dc47665189e
owen <owen@iyes.dev>
2023-05-25 09:51:46 +0800

    y

--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 # 要替換的檔案
 cat k8s-simple/roles/common/files/etcd-config.yaml
 
-ansible-playbook -v -e "host_name=localhost" --extra-vars "etcd_urls=http://test.com:2379,http://test.com:2379,http://test.com:2379"  k8s-simple/site.yml
+ansible-playbook -v -e "host_name=localhost" --extra-vars "etcd_urls=http://test01.com:2379,http://test02.com:2379,http://test03.com:2379"  k8s-simple/site.yml
 ```
 
--- k8s-simple/roles/common/files/replace_etcd_urls.sh
+++ k8s-simple/roles/common/files/replace_etcd_urls.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-SOURCES=($(awk '/^etcd:/ {flag=1; next} flag && /^    - / {gsub(/"/, "", $0); print substr($0, 7)}' roles/common/files/etcd-config.yaml))
+CONFIG_PATH=roles/common/files/etcd-config.yaml
+SOURCES=($(awk '/^etcd:/ {etcd_flag=1; next} etcd_flag && /host:/ {host_flag=1; next} host_flag && /^    - / {gsub(/"/, "", $0); print substr($0, 7)}' ${CONFIG_PATH}))
 IFS=',' read -ra TARGETS <<< "$1"
 
-if [[ ${#TARGETS[@]} -ne ${#SOURCES[@]} ]]; then
+if [ ${#TARGETS[@]} -ne ${#SOURCES[@]} ]; then
   echo "Error: Number of targets doesn't match the number of sources."
   exit 1
 fi
 
 for index in "${!SOURCES[@]}"; do
-    sed -i "" "s~${SOURCES[$index]}~${TARGETS[$index]}~" roles/common/files/etcd-config.yaml
+    echo SOURCES[$index]
+    sed -i "" "s~${SOURCES[$index]}~${TARGETS[$index]}~" ${CONFIG_PATH}
 done
