4b884ca5ddcfa52106e0dd827f9536af8ecb06a8
owen <owen@iyes.dev>
2023-05-25 10:03:49 +0800

    y

--- k8s-simple/roles/common/files/replace_etcd_urls.sh
+++ k8s-simple/roles/common/files/replace_etcd_urls.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CONFIG_PATH=roles/common/files/etcd-config.yaml
-SOURCES=($(awk '/^etcd:/ {etcd_flag=1; next} etcd_flag && /host:/ {host_flag=1; next} host_flag && /^    - / {gsub(/"/, "", $0); print substr($0, 7)}' ${CONFIG_PATH}))
+SOURCES=($(awk '/etcd:/ {etcd_flag=1; next} etcd_flag && /host:/ {host_flag=1; next} host_flag && /\s*- / {gsub(/[-" ]/, "", $0); print}' ${CONFIG_PATH}))
 IFS=',' read -ra TARGETS <<< "$1"
 
 if [ ${#TARGETS[@]} -ne ${#SOURCES[@]} ]; then
@@ -11,5 +11,8 @@ fi
 
 for index in "${!SOURCES[@]}"; do
     echo SOURCES[$index]
+    # macos
     sed -i "" "s~${SOURCES[$index]}~${TARGETS[$index]}~" ${CONFIG_PATH}
+    # linux
+    # sed -i "s~${SOURCES[$index]}~${TARGETS[$index]}~" ${CONFIG_PATH}
 done
